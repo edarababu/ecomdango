9fe4088602f63e3041d06b4ebf8377221410f748
codecrakeratul <59470860+codecrakeratul@users.noreply.github.com>
2020-01-03 11:10:33 +0530

    Update ecom.py

--- ecom.py
+++ ecom.py
@@ -3,13 +3,8 @@ a = 1+2
 print(a)
 print('aaaaaaaabaj')
 print('kkkkkk')
-<<<<<<< HEAD
-
-print('hello')
-
-'darababu sunata'
-=======
 print('hello world')
 print('helloo    again')
 print('hello')
->>>>>>> 0b0154eb9180f505ce257aa5d9c7fa36677f7cd0
+
+"atul"
